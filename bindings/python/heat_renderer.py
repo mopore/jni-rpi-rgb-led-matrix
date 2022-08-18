f27cca71384bd35777127c91a5592a4a11eed3c6
mopore <jni@mopore.org>
2022-08-18 22:51:52 +0200

    Colorization and Interpolation added.

--- bindings/python/heat_renderer.py
+++ bindings/python/heat_renderer.py
@@ -1,57 +1,120 @@
+import json
+import math
+import time
 from rgbmatrix import FrameCanvas
 from renderer import Renderer
+from scipy.interpolate import griddata
+import numpy as np
+
+
+VALUE_RANGE = 100
+MINTEMP = 20.0  # low range of the sensor (this will be blue on the screen)
+MAXTEMP = 35.0  # high range of the sensor (this will be red on the screen)
+
+
+def map_value(x, out_min, out_max):
+	if x < MINTEMP:
+		x = MINTEMP
+	if x > MAXTEMP:
+		x = MAXTEMP
+	return (x - MINTEMP) * (out_max - out_min) / (MAXTEMP - MINTEMP) + out_min
+
+
+def as_sensor_data(dict) -> list[list[float]]:
+	data_array = dict["sensor_data"]
+	return data_array
+
+
+def sensor_data_to_32_32_colored(sensor_data: list[list[float]]) -> np.ndarray:
+	pixels = []
+	for row in sensor_data:
+		pixels = pixels + row
+	pixels = [map_value(p, 0, VALUE_RANGE - 1) for p in pixels]
+
+	# pylint: disable=invalid-slice-index
+	points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+	grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+	# pylint: enable=invalid-slice-index
+
+	# perform interpolation
+	bicubic = griddata(points, pixels, (grid_x, grid_y), method="cubic")
+	return bicubic
+
+
+class DataProvider:
+	def __init__(self) -> None:
+		self.keep_alive = True
+		self.data: str | None = None
+		self.last_date_timestamp: float | None = None
+
+	def store_data(self, text: str) -> None:
+		self.last_date_timestamp = time.monotonic()
+		self.data = text
+
+	def collect_data(self) -> str | None:
+		if self.data is not None:
+			if self.last_date_timestamp is not None:
+				time_passed = time.monotonic() - self.last_date_timestamp
+				if time_passed > 1:
+					self.data = None
+					self.last_date_timestamp = None
+			else:
+				self.data = None
+		return self.data
 
 
 class HeatColorizer:
 
-	def __init__(self, max: int) -> None:
-		self.red: float = 0
-		self.green: float = 0
-		self.blue: float = 0
-		self.max: int = max
+	def __init__(self, data_provider: DataProvider) -> None:
+		json_string = data_provider.collect_data()
+		if (json_string is not None):
+			sensor_data: list[list[float]] = json.loads(json_string, object_hook=as_sensor_data)
+			self.data = sensor_data_to_32_32_colored(sensor_data)
 
-	def get_red(self) -> int:
-		return int(self.red)
+	def get_red(self, row: int, column: int) -> int:
+		if self.data is None:
+			return 0
+		value = self.data[row][column]
+		red_value = 255 * (value / 100)
+		return int(red_value)
 
-	def get_blue(self) -> int:
-		return int(self.blue)
+	def get_blue(self, row: int, column: int) -> int:
+		if self.data is None:
+			return 0
+		value = self.data[row][column]
+		blue_value = 255 - (255 * (value / 100))
+		return int(blue_value)
 
-	def get_green(self) -> int:
-		return int(self.green)
-
-	def update(self, row: int, column: int) -> None:
-		red_share = column / self.max
-		self.red = 255 * red_share
-		blue_share = 1 - (column / self.max)
-		self.blue = 255 * blue_share
+	def get_green(self, row: int, column: int) -> int:
+		return 0
 
 
 class HeatvisionRenderer(Renderer):
 	""" This will show a 32x32 pixel array in the middle of the display with colors from blue to 
 		red """
 
-	CANVAS_WIDTH = 64
-	CANVAS_HEIGHT = 32
+	MATRIX_WIDTH = 64
+	MATRIX_HEIGHT = 32
 
 	ARRAY_WIDTH = 32
 	ARRAY_HEIGHT = 32
 
 	def __init__(self) -> None:
-		self.startx = (HeatvisionRenderer.CANVAS_WIDTH / 2) - (HeatvisionRenderer.ARRAY_WIDTH / 2) 
-		self.starty = (HeatvisionRenderer.CANVAS_HEIGHT / 2) - (HeatvisionRenderer.ARRAY_HEIGHT / 2) 
-		self.colorizer = HeatColorizer(32)
+		self.startx = (HeatvisionRenderer.MATRIX_WIDTH / 2) - (HeatvisionRenderer.ARRAY_WIDTH / 2) 
+		self.starty = (HeatvisionRenderer.MATRIX_HEIGHT / 2) - (HeatvisionRenderer.ARRAY_HEIGHT / 2) 
+		self.data_provider = DataProvider()
 
 	def render(self, offscreen_canvas: FrameCanvas) -> None:
+		colorizer = HeatColorizer(self.data_provider)
 		for row in range(0, HeatvisionRenderer.ARRAY_HEIGHT):
 			for column in range(0, HeatvisionRenderer.ARRAY_WIDTH):
-				self.colorizer.update(row, column)
 				offscreen_canvas.SetPixel(
 					self.startx + column, 
 					self.starty + row, 
-					self.colorizer.get_red(), 
-					self.colorizer.get_green(), 
-					self.colorizer.get_blue()
+					colorizer.get_red(row, column), 
+					colorizer.get_green(row, column),
+					colorizer.get_blue(row, column),
 				)
 	
 	def receive_heatvision_data(self, text: str) -> None:
-		print(f"Data received: {text}")
+		self.data_provider.store_data(text)
