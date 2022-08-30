b6ed37cf9892804ff22afc36ff6630dba315d524
mopore <jni@mopore.org>
2022-08-30 21:53:15 +0200

    Checking for test rig.

--- bindings/python/main.py
+++ bindings/python/main.py
@@ -4,11 +4,22 @@ from heat_renderer import HeatvisionRenderer
 import threading
 from mqtt_bridge import MqttBridge
 import renderer
+import subprocess
 
 ONE_SECOND = 1
 TOPIC_COMMAND_NAME = "jniHome/services/heatvision1/command"
 
 EXIT_COMMAND_NAME = "exit"
+
+
+def test_running_on_test_rig() -> bool:
+    testing_rig = False
+    try:
+        subprocess.check_output(["ip a | grep 192.168.199.247"], shell=True, text=True)
+        testing_rig = True
+    except subprocess.CalledProcessError as e:
+        pass  # We receive an error if IP not matched
+    return testing_rig
 
 
 class RendererShellThread:
@@ -24,11 +35,14 @@ class RendererShellThread:
         options.cols = 64
         options.chain_length = 1
         options.parallel = 1
-        options.hardware_mapping = 'adafruit-hat'
-        options.led_rgb_sequence = 'RGB'
-        #  options.led_rgb_sequence = 'RBG'
-        self.matrix = RGBMatrix(options=options)
+        options.hardware_mapping = "adafruit-hat"
 
+        if test_running_on_test_rig:
+            options.led_rgb_sequence = 'RBG'
+        else:
+            options.led_rgb_sequence = "RGB"
+
+        self.matrix = RGBMatrix(options=options)
         self.selected_renderer_index = 0
         self.heatvision_renderer = HeatvisionRenderer(mqtt_bridge)
         self.renderers: list[renderer.Renderer] = [
@@ -38,7 +52,7 @@ class RendererShellThread:
             # renderer.AnimatedGifRenderer(self.ROCKET_PATH),
         ]
         threading.Thread(target=self.run).start()
-    
+
     def run(self) -> None:
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         frame_counter = 0
@@ -63,28 +77,30 @@ class RendererShellThread:
                 if self.selected_renderer_index == len(self.renderers):
                     self.selected_renderer_index = 0
                 secs_per_scene = 0
-         
+
         print("No more running :(")
 
 
 def main():
     mqtt_bridge = MqttBridge()
     shell = RendererShellThread(mqtt_bridge)
-    
+
     def exit_listener(topic: str, message: str) -> None:
         print("Exit listener triggered. '{topic}' '{message}'")
         if topic == TOPIC_COMMAND_NAME:
             if message == "exit":
                 mqtt_bridge.stop()
                 shell.keep_running = False
-    
+
     mqtt_bridge.subscribe_with_cb(TOPIC_COMMAND_NAME, exit_listener)
-    print(f"On topic '{TOPIC_COMMAND_NAME}' post '{EXIT_COMMAND_NAME}' to stop processing.")
-            
+    print(
+        f"On topic '{TOPIC_COMMAND_NAME}' post '{EXIT_COMMAND_NAME}' to stop processing."
+    )
+
     while shell.keep_running:
         time.sleep(0.1)
     print("All done.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
