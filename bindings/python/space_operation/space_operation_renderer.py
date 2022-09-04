9a3dbdcd7fb2cf10e106d2406a9a0daa85f198b1
mopore <jni@mopore.org>
2022-09-04 12:05:42 +0200

    Reducing FPS

--- bindings/python/space_operation/space_operation_renderer.py
+++ bindings/python/space_operation/space_operation_renderer.py
@@ -6,13 +6,14 @@ from rgbmatrix import FrameCanvas, graphics
 class SpaceOperationRenderer(Renderer):
 
 	TEXT_ORANGE_COLOR = graphics.Color(255, 128, 0)
+	SIXTY_HERTZ = 0.0167
 
 	def __init__(self):
 		self.textColor = self.TEXT_ORANGE_COLOR
 		self.font = graphics.Font()
 		self.font.LoadFont("../../fonts/5x8.bdf")
 		self.textColor = self.TEXT_ORANGE_COLOR
-		self.text = "Space Operation"
+		self.text = "Space OP"
 	
 	def render(self, offscreen_canvas: FrameCanvas) -> None:
 		height = offscreen_canvas.height
@@ -20,4 +21,5 @@ class SpaceOperationRenderer(Renderer):
 		for x in range(width):
 			for y in range(height):
 				offscreen_canvas.SetPixel(x, y, 10, 10, 10)
-		graphics.DrawText(offscreen_canvas, self.font, 0, 20, self.textColor, self.text)
+		graphics.DrawText(offscreen_canvas, self.font, 10, 20, self.textColor, self.text)
+		time.sleep(self.SIXTY_HERTZ)
